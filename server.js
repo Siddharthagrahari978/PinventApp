d2aa37ebd73337114b7ba47b4321594ad000b0ec
Siddharth Kumar <43567482+Siddharthagrahari978@users.noreply.github.com>
2022-12-09 16:40:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://pinvent-app.vercel.app", "https://funny-tarsier-5a3276.netlify.app"],
+    origin: ["http://localhost:3000", "https://pinventapp.netlify.app"],
     credentials: true,
   })
 );
